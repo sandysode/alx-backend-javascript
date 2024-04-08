54179fe5e68aded039c43ab06fcf39690398bc56
sandysode <sandraoghenessode@gmail.com>
2024-04-08 22:31:59 +0000

    es6basics

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (const value of array) {
+    newArr.push(appendString + value);
+  }
+
+  return newArr;
+}
+
